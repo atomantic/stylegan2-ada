5aad2513765161a13c815bd031438d7086099bea
[._.]/ Adam Eivy <70015+atomantic@users.noreply.github.com>
2021-03-30 18:08:50 -0700

    skip saving fakes_init.png

--- training/training_loop.py
+++ training/training_loop.py
@@ -158,8 +158,8 @@ def training_loop(
     grid_latents = np.random.randn(np.prod(grid_size), *G.input_shape[1:])
     grid_fakes = Gs.run(grid_latents, grid_labels,
                         is_validation=True, minibatch_size=minibatch_gpu)
-    save_image_grid(grid_fakes, os.path.join(
-        run_dir, 'fakes_init.png'), drange=[-1, 1], grid_size=grid_size)
+    # save_image_grid(grid_fakes, os.path.join(
+    #     run_dir, 'fakes_init.png'), drange=[-1, 1], grid_size=grid_size)
 
     print(f'Replicating networks across {num_gpus} GPUs...')
     G_gpus = [G]
